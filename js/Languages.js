8b6bef47aec17de49203f1e75d1700967b46e680
Neerosh <torgash.wraith@gmail.com>
2022-05-30 11:48:58 -0300

    test language.js code improvements

---
+++ js/Languages.js
@@ -0,0 +1,13 @@
+var userLang = navigator.language || navigator.userLanguage; 
+
+if (userLang != "pt-BR"){
+    document.documentElement.setAttribute("lang", "en-US");
+    document.getElementById("selectionText").textContent = "Select Your Language:"
+    document.getElementById("englishButton").textContent = "English (en-US)"
+    document.getElementById("portugueseButton").textContent = "Portuguese (pt-BR)"
+}else{
+    document.documentElement.setAttribute("lang", "pt-BR");
+    document.getElementById("selectionText").textContent = "Selecione Sua Linguagem:"
+    document.getElementById("englishButton").textContent = "Inglês (en-US)"
+    document.getElementById("portugueseButton").textContent = "Português (pt-BR)"
+}
